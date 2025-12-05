32173bd13ce3c7c005b25436296d57de4967c611
amadouldiallo <mailsdiallo@gmail.com>
2025-12-05 21:20:53 +0000

    Update POC

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,10 +1,13 @@
 # =============================================================================
 # Terraform Backend Configuration
 # =============================================================================
+# Généré automatiquement par init.sh le 2025-12-05 21:05:09
+# Project: kkgcplabs01-007
+# =============================================================================
 
 terraform {
   backend "gcs" {
-    bucket = "kkgcplabs01-009-tf-state"
+    bucket = "kkgcplabs01-007-tf-state"
     prefix = "terraform/state"
   }
 }
